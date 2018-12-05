85a098fd4fefa6b309b3dd673151e2a013ba6864
Charlie Schmidt <charlie.schmidt@equifax.com>
2018-12-05 10:56:19 -0500

    day 5 part 2

--- day5.ps1
+++ day5.ps1
@@ -7,17 +7,41 @@ begin {
 }
 
 process {
-    $Characters = $InputLines[0].ToCharArray()
 
-    $stack = new-object System.Collections.Stack
+    function React {
+        param($Characters)
+        process {
+            $stack = new-object System.Collections.Stack
 
-    foreach ($c in $Characters) {
-        if ($Stack.Count -gt 0 -and [Math]::Abs(([byte][char]$stack.Peek()) - ([byte][char]$c)) -eq 32) {
-            $Stack.Pop() | Out-Null
-        } else {
-            $Stack.Push($c)
+            foreach ($c in $Characters) {
+                if ($Stack.Count -gt 0 -and [Math]::Abs(([byte][char]$stack.Peek()) - ([byte][char]$c)) -eq 32) {
+                    $Stack.Pop() | Out-Null
+                } else {
+                    $Stack.Push($c)
+                }
+            }
+
+            $Stack.Count | Write-Output    
         }
     }
 
-    Write-Output "Part 1: $($Stack.Count)"
+    $InputCharacters = $InputLines[0].ToCharArray()
+    $Part1 = React -Characters $InputCharacters
+
+    Write-Output "Part 1: $($Part1)"
+
+    $Part2 = $InputLines[0].ToLower().ToCharArray() | Select-Object -Unique | foreach-object {
+        $TryCharacter = $_
+        $TryCharactersLeft = ($InputLines[0] -replace $TryCharacter,'').ToCharArray()
+
+        $TryLength = React -Characters $TryCharactersLeft
+
+        [PSCustomObject]@{
+            Character = $TryCharacter
+            Length = $TryLength
+        } | Write-Output
+    } | Sort-Object -Property Length | Select-Object -First 1 -ExpandProperty Length
+    
+    
+    Write-Output "Part 2: $($Part2)"
 }
